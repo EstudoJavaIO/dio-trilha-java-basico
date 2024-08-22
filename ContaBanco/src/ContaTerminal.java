a3c585401bb511ff7c20daf391d49f92c6a69cea
estudojavadio <estudofrontend24@gmail.com>
2024-08-22 20:57:34 -0300

    exercio conta terminal concluido - DIO

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        Scanner sc = new Scanner(System.in);
+        
+        String agencia, nomeCliente;
+        int numero;
+        
+        System.out.println("Seja Bem-Vindo ao nosso Banco Digital");
+        System.out.println("--------------------------------------------");
+
+        System.out.println("Digite sua Conta: ");
+        numero = sc.nextInt();
+
+        System.out.println("-----------------------------------------");
+
+        System.out.println("Digite Agencia");
+        agencia = sc.next();
+
+        System.out.println("-----------------------------------------");
+
+        System.out.println("Informe seu Nome e Sobrenome");
+        nomeCliente = sc.next();
+
+        sc.next();
+
+        System.out.println("-----------------------------------------");
+
+        System.out.println("Valor a ser depositado");
+        double saldo = sc.nextDouble();
+
+        System.out.println("-----------------------------------------");
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso" + 
+        " banco, sua agência é: " + numero + ", conta " + agencia + " com saldo inicial " + saldo);
+        System.out.println("-----------------------------------------");
+        System.out.println("-----------------------------------------");
+        System.out.println("Obrigado pela preferência e Volte Sempre");
+        System.out.println("-----------------------------------------");
     }
 }
+ 
